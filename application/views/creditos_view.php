4058e27ad24231bcd4cde794accabee122541d38
David Zambrano <dzambrano.226@gmail.com>
2017-12-11 20:59:29 -0500

    se agrega lista de desarrolladores a los creditos

--- application/views/creditos_view.php
+++ application/views/creditos_view.php
@@ -1,3 +1,9 @@
 <div class="mdl">
 	<h1 class="page-header"> Creditos de la aplicacion</h1>
+
+	<li>
+		<ul>Desarrolladores</ul>
+		<ul>David Zambrano</ul>
+		<ul>Astrid rodriguez</ul>
+	</li>
 </div>
